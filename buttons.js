f3deaf08f481d9ea3c97c72f6db11f528775b91b
Andrea Robles <cardavila13@gmail.com>
2022-02-10 17:30:28 +0100

    funcionalidad botones

--- buttons.js
+++ buttons.js
@@ -1,7 +1,8 @@
-windows.onload = inicio;
+window.onload = inicio;
 
 function inicio() {
   document.querySelectorAll(".boton")[0].onclick=action1;
+  document.querySelectorAll(".boton")[1].onclick=action2;
 }
 
 function action1() {
